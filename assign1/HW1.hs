c056bc746908405aa00a9293365cd07e4a097929
DrakeSeifert <drakeseifert1@gmail.com>
2017-04-26 18:14:47 -0700

    Finished 2

--- assign1/HW1.hs
+++ assign1/HW1.hs
@@ -42,9 +42,9 @@ instance Show Mode where
 
 {-
 Q1 b
-Write a Mini Logo macro vector that draws a line from a given position 
-(x1,y1) to a given position (x2,y2) and represent the macro in abstract
-syntax, that is, as a Haskell data type value.
+Write a Mini Logo macro vector that draws a line from a given
+position (x1,y1) to a given position (x2,y2) and represent the
+macro in abstract syntax, that is, as a Haskell data type value.
 -}
 
 vector = Def "vector"
@@ -77,3 +77,82 @@ steps n = 			Seq [
 						 Call "vector" [n-1, n, n, n]
 						]
 					]
+
+{-
+Q2 a
+Define abstract syntax for Digital Circuit as a Haskell data type
+-} 
+
+--Define abstract syntax
+--NOTE: Num defined above on line 12
+data Circuit = Con Gates Links      
+
+data Gates = GCon [(Num, GateFN)]
+
+data GateFN = And
+            | Or
+            | Xor
+            | Not
+
+data Links = LCon [((Num, Num), (Num, Num))]
+
+{-
+Q2 b
+Represent half adder circuit as Haskell Data type value
+-}
+halfAdder = Con (GCon [(1, Xor), (2, And)])
+            (LCon [((1,1),(2,1)), ((1,2),(2,2))])
+
+{-
+Q2 c
+Write PPrint for circuit
+-}
+
+{-
+Q3 a
+Represent the expression -(3+4)*7 in the alternative abstract
+syntax.
+-}
+
+data Expr = N Int
+	| Plus Expr Expr
+	| Times Expr Expr
+	| Neg Expr
+	deriving Show
+
+data Op = Add | Multiply | Negate
+	deriving Show
+
+data Exp = Num Int
+	| Apply Op [Exp]
+	deriving Show
+
+q3a = Apply Multiply [Apply Negate [Apply Add [Num 3, Num 4]], Num 7]
+
+{-
+Q3 b
+What are the advantages or disadvantages of either representation?
+
+Personally, I feel like the first representation is easier to read
+and understand, and flows a lot more like written math. The
+second representation is a little more compact with the downside
+of being a little more challenging to think about conceptually
+when dealing with multiple operators. The second method however
+can also take entire lists of numbers to an operator at once which
+could be more useful for specific applications where an operator is
+mapped across a large amount of operands. Whichever one is best
+really depends on preference and the given scenario.
+-}
+
+{-
+Q3 c
+Define a function translate :: Expr -> Exp that translates
+expressions given in the first abstract syntax into equivalent
+expressions in the second abstract syntax.
+-}
+
+translate :: Expr -> Exp
+translate (N n) = (Num n)
+translate (Plus n m) = Apply Add [(translate n), (translate m)]
+translate (Times n m) = Apply Multiply [(translate n), (translate m)]
+translate (Neg n) = Apply Negate [translate n]
