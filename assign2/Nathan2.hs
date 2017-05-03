295d8e4c3fab5747a21bfc5191b9539b4319b9dd
Nathan Shepherd <shephern@flip1.engr.oregonstate.edu>
2017-05-03 13:38:42 -0700

    Added more test cases

--- assign2/Nathan2.hs
+++ assign2/Nathan2.hs
@@ -7,6 +7,7 @@ module Nathan2 where
 {-
    - Ex. 1
    - Stack Language
+   - Usage: eval Prog
 -}
 
 --Abstract syntax
@@ -31,21 +32,22 @@ sem (x:xs) s = sem xs (semCmd x s)
 semCmd :: Cmd -> D
 semCmd (LD i) s = (i : s)
 semCmd ADD s = case length s of
-        0 -> error("ADD ERROR")
-        1 -> error("ADD ERROR")
+        0 -> error("ADD ERROR-- Cannot perform on empty list")
+        1 -> error("ADD ERROR-- Cannot perform on signleton list")
         _ -> (sum(take 2 s) : drop 2 s)
 semCmd MULT s = case length s of
-        0 -> error("MULT ERROR")
-        1 -> error("MULT ERROR")
+        0 -> error("MULT ERROR-- Cannot perform on empty list")
+        1 -> error("MULT ERROR-- Cannot perform on singleton list")
         _ -> (product(take 2 s) : drop 2 s)
 semCmd DUP s = case length s of
-        0 -> error("DUP ERROR")
+        0 -> error("DUP ERROR-- Cannot perform on empty list")
         _ -> ((head s) : s)
 
 --Must be used so that GHCi can show the [Int]
 eval :: Prog -> Stack
 eval pro = sem pro []
 
+--Defined test cases
 p1::Prog
 p1 = [LD 3,DUP,ADD,DUP,MULT]
 
@@ -54,5 +56,19 @@ p2 = [LD 3,ADD]
 
 p3::Prog
 p3 = []
+
+p4::Prog
+p4 = [MULT, LD 2]
+
+p5::Prog
+p5 = [LD 3,DUP,ADD,ADD]
+
+p6::Prog
+p6 = [LD 4,DUP,DUP,ADD,MULT,LD 7,ADD]
+
+{-
+   - Ex. 2
+   - 
+-}
 
 
