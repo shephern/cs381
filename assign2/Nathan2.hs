8f4f0a4ea3dace26ed0dcec55f1db51aa9da7615
Nathan Shepherd <shephern@flip1.engr.oregonstate.edu>
2017-05-03 13:17:40 -0700

    Fully functioning and error checking part 1

--- assign2/Nathan2.hs
+++ assign2/Nathan2.hs
@@ -33,15 +33,26 @@ semCmd (LD i) s = (i : s)
 semCmd ADD s = case length s of
         0 -> error("ADD ERROR")
         1 -> error("ADD ERROR")
-        _ -> (sum(take 2 s) : s)
+        _ -> (sum(take 2 s) : drop 2 s)
 semCmd MULT s = case length s of
         0 -> error("MULT ERROR")
         1 -> error("MULT ERROR")
-        _ -> (product(take 2 s) : s)
+        _ -> (product(take 2 s) : drop 2 s)
 semCmd DUP s = case length s of
         0 -> error("DUP ERROR")
         _ -> ((head s) : s)
 
-p::Prog
-p = [LD 3,DUP,ADD,DUP,MULT]
+--Must be used so that GHCi can show the [Int]
+eval :: Prog -> Stack
+eval pro = sem pro []
+
+p1::Prog
+p1 = [LD 3,DUP,ADD,DUP,MULT]
+
+p2::Prog
+p2 = [LD 3,ADD]
+
+p3::Prog
+p3 = []
+
 
