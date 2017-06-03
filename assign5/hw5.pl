aa5a8cb229eb4b98fe559f89f7d4121efa087493
DrakeSeifert <drakeseifert1@gmail.com>
2017-06-02 21:08:21 -0700

    Completed 1a, 1b, 1c

--- assign5/hw5.pl
+++ assign5/hw5.pl
@@ -10,15 +10,24 @@ when(261,12).
 when(381,11).
 when(398,12).
 when(399,12).
+
 where(275,owen102).
 where(261,dear118).
 where(381,cov216).
 where(398,dear118).
 where(399,cov216).
+
 enroll(mary,275).
 enroll(john,275).
 enroll(mary,261).
 enroll(john,381).
 enroll(jim,399).
 
-%Exercise 1a
+%1a
+schedule(X,Y,Z) :- enroll(X,A), where(A,Y), when(A,Z).
+
+%1b
+usage(X,Y) :- where(A,X), when(A,Y).
+
+%1c
+conflict(X,Y) :- where(X,A), where(Y,A), when(X,B), when(Y,B), X\=Y.
