b5a4e22421c53e5733794f2ab5ca2a21c92c4a83
DrakeSeifert <drakeseifert1@gmail.com>
2017-06-02 20:28:37 -0700

    Started HW5

--- assign5/hw5.pl
+++ assign5/hw5.pl
@@ -1,2 +1,24 @@
-%comment
+/*
+CS381 HW5
+Drake Seifert (seifertd)
+Nathan Shepherd (shephern)
+Justin Chang (changle)
+*/
+
 when(275,10).
+when(261,12).
+when(381,11).
+when(398,12).
+when(399,12).
+where(275,owen102).
+where(261,dear118).
+where(381,cov216).
+where(398,dear118).
+where(399,cov216).
+enroll(mary,275).
+enroll(john,275).
+enroll(mary,261).
+enroll(john,381).
+enroll(jim,399).
+
+%Exercise 1a
