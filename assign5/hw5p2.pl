669a4b92adae28047879486f9f6a5eaf99b9c5ae
chang5cal <ginka13@gmail.com>
2017-06-05 15:42:37 -0700

    Modified part 3

--- assign5/hw5p2.pl
+++ assign5/hw5p2.pl
@@ -10,7 +10,9 @@ rdup([H|L],[H|M]) :- not(member(H,L)), rdup(L,M).
 rdup([H|L],M) :- member(H,M), rdup(L,M).
 %If it's not unique, then ignore it and go
 
-%If just member, rdup. If not, append it then rdup.
+%Note: This code was mostly copied from
+%https://stackoverflow.com/questions/2260792/remove-duplicates-in-list-prolog
+%with some modification (Base case is different), at the suggestion of a TA.
 
 %b
 flat([],_).
@@ -27,12 +29,18 @@ flat(H,[H|_]).
 %Take off the head, otherwise append it. Then hopefully recursion goes next head
 
 %c
-suball([H|T]) :- H-1,suball(T).
+project(_,[],_).
+project([],_,_).
+%Base cases, if either lists are empty, we're done.
 
-project(X,[_|Y],L) :- suball(X),project([X,Y,L]).
-project([0|T],[H|Y],[H|L]) :- project(T,Y,L).
+project(0,[H|_],[H|_]).
+%Another base case. If X is 0, add the current head of Y to L.
 
+project(X,[_|Y],L) :- project(X-1,Y,L).
+%This basically just traverses Y X times. It'll trigger the second base case if
+%it can't be done, and third if it can.
 
-
-%Keep subtracting 1 from the list until the head is 0, then remove that
-%head. Repeat until it's over.
+project([H|X],Y,L) :- project(H-1,Y,L), project(X,Y,L).
+%The head of X gets subtracted by one and sent in to itself to use
+%the above 2 clauses, then it goes to the next head on the tail.
+%It will trigger the first base case.
