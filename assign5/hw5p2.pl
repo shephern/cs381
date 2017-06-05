db4f74c0603fdccc9fec9a13bcf162203d241b98
chang5cal <ginka13@gmail.com>
2017-06-05 13:46:28 -0700

    Fixed syntax errors

--- assign5/hw5p2.pl
+++ assign5/hw5p2.pl
@@ -1,24 +1,28 @@
 /* Exercise 2 */
 %a
-rdup([H|L],M) :- member(H,M), rdup(L,M)
-rdup([H|L],M) :- \+ member(H,M), append(M,H,M), rdup(L,M)
+rdup([_|L],M) :- rdup(L,M).
+rdup([H|_],M) :- not(member(H,M)), append(M,H,M).
 
-%So: If member, rdup. If not, append it them rdup.
-
+%If just member, rdup. If not, append it then rdup.
 %b
-flat([H|L],F) :- flat(H,F), flat(L,F)
-flat(L,F) :- append(F,H,F)
+
+flat([H|L],F) :- flat(H,F), flat(L,F).
+flat(L,F) :- append(F,L,F).
+flat([],F) :- F.
 
 %Append if no other list, else move in to the list.
 %Hopefully doing this right: First case is list, second is not. If list, just
 %Take off the head, otherwise append it. Then hopefully recursion goes next head
 
 %c
-project(X,[H,Y],L) :- suball(X),project([X,Y,L)
-project([X,T],[H,Y],L) :- x=0,append(L,H,L),project(T,Y,L)
-suball([H|T]) :- H-1,suball(T)
+suball([H|T]) :- H-1,suball(T).
 
-%So if it only increases, I could probably just recurse through the thing once
-%and if the amount of recursions equal the head of X, append it to
-%Okay so: Keep subtracting 1 from the list until the head is 0, then remove that
+project(X,[_|Y],L) :- suball(X),project([X,Y,L]).
+project([X|T],[H|Y],L) :- X=0,append(L,H,L),project(T,Y,L).
+project(_,[],L) :- L.
+project([],_,L) :- L.
+
+
+
+%Keep subtracting 1 from the list until the head is 0, then remove that
 %head. Repeat until it's over.
